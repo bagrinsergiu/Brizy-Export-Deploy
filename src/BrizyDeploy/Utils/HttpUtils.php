8b31864a9cf103a4ac4d4d7900bc53cf290a98fa
Andrei Bodnar <bodnar1212@gmail.com>
2019-02-27 15:32:23 +0200

    fix minor

--- src/BrizyDeploy/Utils/HttpUtils.php
+++ src/BrizyDeploy/Utils/HttpUtils.php
@@ -8,15 +8,16 @@ class HttpUtils
 {
     static public function getBaseUrl(Request $request, $route_from, $route_to)
     {
-        $prefix = str_replace($route_from, '', $request->getBaseUrl());
-        if ($prefix != '') {
-            $baseUrl = $request->getScheme() . '://' . $request->getHost() . $prefix . $route_to;
-        } else {
-            $baseUrl = $request->getScheme() . '://' . $request->getHost() . $route_to;
-        }
-
+        $baseUrl = $request->getScheme() . '://' . $request->getHost();
         if ($request->getPort() != 80 && $request->getPort() != 443) {
             $baseUrl .= ':' . $request->getPort();
+        }
+
+        $prefix = str_replace($route_from, '', $request->getBaseUrl());
+        if ($prefix != '') {
+            $baseUrl = $baseUrl . $prefix . $route_to;
+        } else {
+            $baseUrl = $baseUrl . $route_to;
         }
 
         return $baseUrl;
